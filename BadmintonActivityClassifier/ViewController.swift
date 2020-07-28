4f9005330d886ac0232549a0dc6e5bd9b4933fe9
ekirifaldi <ekirifaldi2010@gmail.com>
2020-07-28 21:33:35 +0800

    new project

---
+++ BadmintonActivityClassifier/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BadmintonActivityClassifier
+//
+//  Created by Eki Rifaldi on 28/07/20.
+//  Copyright © 2020 Eki Rifaldi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
